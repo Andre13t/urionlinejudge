1ae5215a57d071d5fdcf7ed3784da901fe354a38
Andre13t <53703813+Andre13t@users.noreply.github.com>
2020-08-12 19:06:07 -0300

    Começo

---
+++ 1001.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int A, B;
+
+	std::cin >> A >> B;
+	std::cout << "X = " << A + B << std::endl;
+
+	return 0;
+}
---
+++ 1002.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+ 
+int main() {
+ 
+	long double raio, n = 3.14159;
+	cin >> raio;
+	cout << fixed << setprecision(4);
+	cout << "A=" << n * (raio * raio) << endl;
+	return 0;
+}
